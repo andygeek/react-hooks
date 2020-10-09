577a0e5a70eba5eec04d2cb1bfcd1f841b752e2c
AndyGeek <andy.eulogio@gmail.com>
2020-10-09 11:48:41 -0500

    Basic counter using useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { CounterApp } from './components/CounterApp.jsx';
 
 export const App = () => {
+	console.log("Renderizando primer componente");
 	return (
 		<div className="container">
-			<h1>Hola mundo</h1>
+			<CounterApp />
 		</div>
 	);
 };
