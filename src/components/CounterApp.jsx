b5f4c907316b5e0617c55c5fe567a6ae0c68a001
AndyGeek <andy.eulogio@gmail.com>
2020-10-09 16:09:21 -0500

    Multiple counter with useState

--- src/components/CounterApp.jsx
+++ src/components/CounterApp.jsx
@@ -1,17 +1,31 @@
 import React, { useState } from 'react';
 
 export const CounterApp = () => {
-	const [counter, setCounter] = useState(0);
-    console.log('Renderizando componente counter');
+	const [counter, setCounter] = useState({
+		counter1: 10,
+		counter2: 20,
+		counter3: 30,
+	});
+	console.log(':D');
+	// Destructuring
+	const { counter1, counter2, counter3 } = counter;
+
 	return (
 		<>
 			<h1>Counter App</h1>
 			<hr />
-			<h3>Counter : {counter}</h3>
+			<h3>Counter1 : {counter1}</h3>
+			<h3>Counter2 : {counter2}</h3>
+			<h3>Counter3 : {counter3}</h3>
 			<button
 				className="btn btn-primary mr-2"
 				onClick={() => {
-					setCounter(counter - 1);
+					setCounter({
+						...counter,
+						counter1: counter1 - 1,
+						counter2: counter2 - 2,
+						counter3: counter3 - 3,
+					});
 				}}
 			>
 				-1
@@ -19,11 +33,17 @@ export const CounterApp = () => {
 			<button
 				className="btn btn-success"
 				onClick={() => {
-					setCounter(counter + 1);
+					setCounter({
+						...counter,
+						counter1: counter1 + 1,
+						counter2: counter2 + 2,
+						counter3: counter3 + 3,
+					});
 				}}
 			>
 				+1
 			</button>
+
 		</>
 	);
 };
