a3ca9f327aa28debb2a0c9b7f1d3ec9ba8fae5ac
AndyGeek <andy.eulogio@gmail.com>
2020-10-10 20:14:04 -0500

    useReducer using count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+
+const ACTIONS = {
+	INCREMENT: 'increment',
+	DECREMENT: 'decrement'
+}
+
+function reducer(state, action) {
+	switch (action.type) {
+		case ACTIONS.INCREMENT:
+			return { count: state.count + 1 };
+		case ACTIONS.DECREMENT:
+			return { count: state.count - 1 };
+		default:
+			return state;
+	}
+}
 
 export const App = () => {
+	const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+	const increment = () => {
+		dispatch({ type: ACTIONS.INCREMENT });
+	};
+
+	const decrement = () => {
+		dispatch({ type: ACTIONS.DECREMENT });
+	};
+
 	return (
-		<div className="container">
-			<h1>Hola mundo</h1>
-		</div>
+		<>
+			<button onClick={decrement}>-</button>
+			<span>{state.count}</span>
+			<button onClick={increment}>+</button>
+		</>
 	);
 };
